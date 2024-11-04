6b3a091d7ddaf95f5de39229babc0e5c859ce533
Thomas Callewaert <92819334+ThomasCall@users.noreply.github.com>
2024-11-04 12:28:45 +0100

    Add function to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
     if(choice == 0){
@@ -10,7 +13,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(){
-    let choice = prompt("Rock, paper or scissors?")
+    let choice = prompt("Rock, paper or scissors?").toLowerCase()
     if(choice == "rock"){
         return "rock"
     } else if(choice == "paper"){
@@ -22,5 +25,56 @@ function getHumanChoice(){
     }
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice){
+    switch(humanChoice) {
+        case "rock":
+            if(computerChoice == "paper"){
+                console.log("You lose! Paper beats Rock")
+                computerScore++
+                break;
+            }
+            else if (computerChoice == "scissors"){
+                console.log("I win! Rock beats Scissors")
+                humanScore++
+                break;
+            }
+            else{
+                console.log("No winner!")
+                break;
+            }
+        case "paper":
+            if(computerChoice == "scissors"){
+                console.log("You lose! Scissors beats Paper")
+                computerScore++
+                break;
+            }
+            else if (computerChoice == "rock"){
+                console.log("I win! Paper beats Rock")
+                humanScore++
+                break;
+            }
+            else{
+                console.log("No winner!")
+                break;
+            }
+        case "scissors":
+            if(computerChoice == "rock"){
+                console.log("You lose! Rock beats Scissors")
+                computerScore++
+                break;
+            }
+            else if (computerChoice == "paper"){
+                console.log("I win! Scissors beats Paper")
+                humanScore++
+                break;
+            }
+            else{
+                console.log("No winner!")
+                break;
+            }
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
